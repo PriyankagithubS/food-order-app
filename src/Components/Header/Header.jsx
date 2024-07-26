7beca0a6889d69739c8edb0ef6a053344feb26e9
PriyankagithubS <priyankashanmugasundaramr@gmail.com>
2024-07-26 11:43:11 +0530

    error correction

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import './Header.css'
-import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
+
 const Header = () => {
   return (
     <div className='header'>
     <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
-        <button onClick={() => navigate('/Menu') }>View Menu</button>
+        <button >View Menu</button>
     </div>
     </div>
   )
