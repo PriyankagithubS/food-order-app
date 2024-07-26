f5875aac2481c4576a97f65895159df7b544f1c3
PriyankagithubS <priyankashanmugasundaramr@gmail.com>
2024-07-26 11:41:54 +0530

    navigation

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './Header.css'
+import { useNavigate } from 'react-router-dom';
+const navigate = useNavigate();
 const Header = () => {
   return (
     <div className='header'>
